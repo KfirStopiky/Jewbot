906e32fba14033c0c7a0a785aa3550230fce5dc6
Kfir <stopiky2@gmail.com>
2021-10-11 19:28:04 +0300

    modals components fixed

--- jewbot-last-version/src/app/components/header/header.component.ts
+++ jewbot-last-version/src/app/components/header/header.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SwapService } from 'src/app/shared/swap/swap.service';
+import { SwapComponent } from '../swap/swap.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(public _Router: Router, public _SwapServicse: SwapService) {}
 
-  constructor(public _Router: Router) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
